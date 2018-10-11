275f514259e35d161aa788a31dc8f243946ef0ed
crusso <chiara.russo@objectway.it>
2018-10-11 12:59:04 +0200

    added figure model

---
+++ tris-core/src/java/model/Figure.java
@@ -0,0 +1,27 @@
+package java.model;
+
+public class Figure {
+
+    private String x;
+    private String y;
+    private Boolean isCircle;
+
+    public String getX() {
+        return x;
+    }
+    public void setX( final String x ) {
+        this.x = x;
+    }
+    public String getY() {
+        return y;
+    }
+    public void setY( final String y ) {
+        this.y = y;
+    }
+    public Boolean getCircle() {
+        return isCircle;
+    }
+    public void setCircle( final Boolean circle ) {
+        isCircle = circle;
+    }
+}
