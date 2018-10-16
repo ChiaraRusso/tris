35c20658a2ac6ba87e7cf939ae853eee2806425c
Chiara <chiara.russo@objectway.it>
2018-10-16 15:04:06 +0200

    rf

--- tris-core/src/chiara/controller/Controller.java
+++ tris-core/src/chiara/controller/Controller.java
@@ -3,40 +3,31 @@ package chiara.controller;
 
 import chiara.model.Circle;
 import chiara.model.Cross;
-import chiara.model.Figure;
 
 public class Controller {
 
     public static final Integer DIM = 3;
 
-    private Circle circle;
-    private Cross cross;
-    private Boolean[][] field;
-
-    public Controller( final Circle circle, final Cross cross, final Boolean[][] field ) {
-        this.circle = circle;
-        this.cross = cross;
-        this.field = field;
-    }
+    private String[][] field;
 
     public Controller() {
-        this.field = new Boolean[ DIM ][ DIM ];
-        this.circle = new Circle();
-        this.cross = new Cross();
+        this.field = new String[ DIM ][ DIM ];
+
+        for ( int i = 0; i < DIM; i++ ) {
+            for ( int j = 0; j < DIM; j++ )
+                field[ i ][ j ] = "";
+        }
     }
 
     // x and y are the position you want to go
-    public void move( final Boolean[][] field, Figure f, final Integer x, final Integer y ) {
+    public void move( final String [][] field,final String player, final Integer x, final Integer y ) {
 
         for ( int i = 0; i < DIM; i++ ) {
             for ( int j = 0; j < DIM; j++ ) {
                 if ( i == x ) {
                     if ( j == y ) {
-                        if ( !field[ x ][ y ] ) {
-
-                            field[ x ][ y ] = true;
-                            f.setX( x );
-                            f.setY( y );
+                        if (field[ x ][ y ].equals( "" ) ) {
+                            field[ x ][ y ] = player;
                         } else
                             System.out.println( "Position is already occupied" );
                     }
@@ -47,23 +38,29 @@ public class Controller {
     }
 
 
+    public void printMatrix() {
+
+        for ( int i = 0; i < DIM; i++ ) {
+            for ( int j = 0; j < DIM; j++ )
+                System.out.println( field[ i ][ j ] );
+            System.out.println();
+        }
+    }
+
+    public void printAvailablePositions(){
+        for(int i=0; i<DIM; i++){
+            for(int j=0; j<DIM; j++)
+            {
+                System.out.println("("+i+"-"+j+")");
+            }
+        }
+    }
+
     public boolean isOver() {
         return true;
     }
 
-    public Circle getCircle() {
-        return circle;
-    }
-    public void setCircle( final Circle circle ) {
-        this.circle = circle;
-    }
-    public Cross getCross() {
-        return cross;
-    }
-    public void setCross( final Cross cross ) {
-        this.cross = cross;
-    }
-    public static Integer getDIM() {
-        return DIM;
+    public String[][] getField() {
+        return field;
     }
 }
--- tris-core/src/chiara/main/MainNoGI.java
+++ tris-core/src/chiara/main/MainNoGI.java
@@ -10,31 +10,31 @@ import static chiara.controller.Controller.DIM;
 public class MainNoGI {
 
     private static Controller controller;
+    private static String player;
+
+    private static String x;
+    private static String y;
 
     public static void main( String[] args ) throws Exception {
 
         controller = new Controller();
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hi. Enter your name..");
+        Scanner in = new Scanner( System.in );
+        System.out.println( "Hi. Enter your name.." );
         String name = in.next();
-        System.out.println("Hi "+name+", and welcome to Tic-Tac-Toe.");
+        System.out.println( "Hi " + name + ", and welcome to Tic-Tac-Toe." );
         System.out.println();
-        System.out.println("Press 1 if you want to play with the x item or press 2 if you want to play with the o item");
-        String guest = in.next();
-        if(guest.equals( "1" ))
-            guest = "x";
+        System.out.println( "Press 1 if you want to play with the x item or press 2 if you want to play with the o item" );
+        player = in.next();
+        if ( player.equals( "1" ) )
+            player = "x";
         else
-            guest = "o";
-        System.out.println("Congratulation, you picked the "+guest+"!");
+            player = "o";
+        System.out.println( "Congratulation, you picked the " + player + "!" );
+        System.out.println("Ready to play?");
+        System.out.println("Here the positions available:");
+        controller.printMatrix();
     }
 
 
-    private void printMatrix(Boolean[][] field){
-
-    }
-
-    private void printPositionAvailable(Boolean[][] field){
-
-    }
 }
