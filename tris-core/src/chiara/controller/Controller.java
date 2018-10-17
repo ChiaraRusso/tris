7e98cc6638df02e050d615d499bf049ebf804f05
chiara <chiara.russo@objectway.it>
2018-10-17 16:08:32 +0200

    added check on win

--- tris-core/src/chiara/controller/Controller.java
+++ tris-core/src/chiara/controller/Controller.java
@@ -1,6 +1,10 @@
 package chiara.controller;
 
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.awt.*;
+
 public class Controller {
 
     public static final Integer DIM = 3;
@@ -16,8 +20,8 @@ public class Controller {
         }
     }
 
-    // x and y are the position you want to go
-    public void move( final String player, final int x, final int y ) {
+    // x and y are the position you want to move to
+    public Boolean move( final String player, final int x, final int y ) {
 
         for ( int i = 0; i < DIM; i++ ) {
             for ( int j = 0; j < DIM; j++ ) {
@@ -25,45 +29,115 @@ public class Controller {
                     if ( j == y ) {
                         if ( field[ x ][ y ].equals( " " ) ) {
                             field[ x ][ y ] = player;
-                        } else
-                            System.out.println( "Position is already occupied" );
+                            return true;
+                        }
+                        return false;
+
                     }
                 }
             }
         }
-
+        return false;
     }
 
     public void printMatrix() {
 
         System.out.println();
         String fs = String.format(
+                "\t  %s | %s | %s  \n" +
+                        "\t-------------\n" +
                         "\t  %s | %s | %s  \n" +
-                        "\t-------------\n"    +
-                        "\t  %s | %s | %s  \n" +
-                        "\t-------------\n"+
+                        "\t-------------\n" +
                         "\t  %s | %s | %s  \n\n",
-                field[ 0 ][ 0 ],field[ 0 ][ 1 ],field[ 0 ][ 2 ],
-                field[ 1 ][ 0 ],field[ 1 ][ 1 ],field[ 1 ][ 2 ],
-                field[ 2 ][ 0 ],field[ 2 ][ 1 ],field[ 2 ][ 2 ]
+                field[ 0 ][ 0 ], field[ 0 ][ 1 ], field[ 0 ][ 2 ],
+                field[ 1 ][ 0 ], field[ 1 ][ 1 ], field[ 1 ][ 2 ],
+                field[ 2 ][ 0 ], field[ 2 ][ 1 ], field[ 2 ][ 2 ]
         );
 
-        System.out.println(fs);
+        System.out.println( fs );
 
     }
 
-    public void printAvailablePositions(Thread t) throws InterruptedException {
+    public void printAvailablePositions( Thread t ) throws InterruptedException {
         for ( int i = 0; i < DIM; i++ ) {
             for ( int j = 0; j < DIM; j++ ) {
-                if ( field[ i ][ j ].equals( " " ) ){
+                if ( field[ i ][ j ].equals( " " ) ) {
                     t.sleep( 500 );
-                    System.out.println( "(" + i + "-" + j + ")" );}
+                    System.out.print( "(" + i + "-" + j + ")" );
+                }
             }
+            System.out.println();
         }
     }
 
-    public boolean isOver() {
-        return true;
+    public Boolean win( String p ) {
+
+        if ( horizontalCheck( p ) || verticalCheck( p ) || middleCheck( p ) || middleCheck( p ) || inverseMiddleCheck( p ))
+            return true;
+        return false;
+    }
+
+    public Boolean horizontalCheck( String p ) {
+        int j = 0;
+        for ( int i = 0; i < DIM; i++ ) {
+            if ( field[ i ][ j ].equals( p ) ) {
+                if ( field[ i ][ j + 1 ].equals( p ) ) {
+                    if ( field[ i ][ j + 2 ].equals( p ) )
+                        return true;
+                    else
+                        j = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public Boolean verticalCheck( String p ) {
+        int i = 0;
+        for ( int j = 0; j < DIM; j++ ) {
+            if ( field[ i ][ j ].equals( p ) ) {
+                if ( field[ i + 1 ][ j ].equals( p ) ) {
+                    if ( field[ i + 2 ][ j ].equals( p ) )
+                        return true;
+                    else
+                        i = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Boolean middleCheck( String p ) {
+        int c = 0;
+        for ( int i = 0; i < DIM; i++ ) {
+            for ( int j = 0; j < DIM; j++ ) {
+                if ( i == j ) {
+                    if ( field[ i ][ j ].equals( p ) ) {
+                        c++;
+                    }
+                }
+            }
+        }
+        if ( c == 3 )
+            return true;
+        return false;
+    }
+
+    public Boolean inverseMiddleCheck( String p ) {
+        int c = 0;
+        for ( int i = 0; i < DIM; i++ ) {
+            for ( int j = 0; j < DIM; j++ ) {
+                if ( i + j == DIM - 1 ) {
+                    if ( field[ i ][ j ].equals( p ) ) {
+                        c++;
+                    }
+                }
+            }
+        }
+        if ( c == 3 )
+            return true;
+        return false;
     }
 
     public String[][] getField() {
--- tris-core/src/chiara/main/MainNoGI.java
+++ tris-core/src/chiara/main/MainNoGI.java
@@ -34,49 +34,97 @@ public class MainNoGI {
         t.sleep( 1500 );
         System.out.println( "Press 1 if you want to play with the x item or press 2 if you want to play with the o item" );
         player = in.next();
+        while ( !checkValidPlayerOption( player ) ) {
+            System.out.println();
+            System.out.println( "Uh-oh! The number you insert is not valid." );
+            System.out.println();
+            System.out.println( "Press 1 if you want to play with the x item or press 2 if you want to play with the o item" );
+            player = in.next();
+        }
         if ( player.equals( "1" ) ) {
             player = "x";
             opponent = "o";
-        } else if(player.equals( "2" )){
+        } else if ( player.equals( "2" ) ) {
             player = "o";
             opponent = "x";
         }
         t.sleep( 500 );
         System.out.println( "Congratulation, you picked the " + player + "!" +
-                "Your opponent will have the "+opponent );
+                "Your opponent will play with the " + opponent );
         System.out.println();
         t.sleep( 900 );
-        System.out.println( "~~~~~ Ready to play? ~~~~~" );
+        System.out.println( "~~~~~~~ Ready to play? ~~~~~~~" );
         System.out.println();
         t.sleep( 2000 );
-        System.out.println( "Here the positions available:" );
-        t.sleep( 500 );
-        System.out.println( " X-Y " );
-        controller.printAvailablePositions(t);
-        t.sleep( 500 );
-        controller.printMatrix();
-        System.out.println( "Enter your X position:" );
-        x = in.nextInt();
-        System.out.println( "Enter your Y position:" );
-        y = in.nextInt();
-        t.sleep( 700 );
-        controller.move( player, x, y );
-        controller.printMatrix();
-        System.out.println();
-        t.sleep( 500 );
-        System.out.println("--- opponent move ---");
-        System.out.println();
-        t.sleep( 1500 );
-        aiController.generateOpponentPosition( controller.getField(),opponent );
-        controller.printMatrix();
 
+        for ( int i = 0; i < 3; i++ ) {
 
+            System.out.println( "Here the positions available:" );
+            t.sleep( 500 );
+            System.out.println( " X-Y " );
+            controller.printAvailablePositions( t );
+            t.sleep( 500 );
+            controller.printMatrix();
+            System.out.println( "Enter your X position:" );
+            x = in.nextInt();
+            System.out.println( "Enter your Y position:" );
+            y = in.nextInt();
+            while ( !checkValidCoordinate( x ) || !checkValidCoordinate( y ) ) {
+                System.out.println();
+                System.out.println( "Uh-oh! The Coordinates you insert are not valid." );
+                t.sleep( 600 );
+                System.out.println( "Enter your X position:" );
+                x = in.nextInt();
+                System.out.println( "Enter your Y position:" );
+                y = in.nextInt();
+            }
+            if ( controller.move( player, x, y ) ) {
+                t.sleep( 700 );
+                controller.printMatrix();
+            } else {
+                while ( !controller.move( player, x, y ) ) {
+                    System.out.println();
+                    System.out.println( "Uh-oh! The Coordinates you insert are not valid. That position is no long available." );
+                    System.out.println();
+                    t.sleep( 600 );
+                    System.out.println( "Enter your X position:" );
+                    x = in.nextInt();
+                    System.out.println( "Enter your Y position:" );
+                    y = in.nextInt();
+                }
+                t.sleep( 700 );
+                controller.printMatrix();
+            }
+
+            System.out.println();
+            t.sleep( 500 );
+            System.out.println( "--- opponent move ---" );
+            System.out.println();
+            t.sleep( 1500 );
+            aiController.generateOpponentPosition( controller.getField(), opponent );
+            controller.printMatrix();
+            if(controller.win( player ) || controller.win( opponent ))
+                System.out.println("WON");
+        }
+
+    }
+
+    public static Boolean checkValidCoordinate( int x ) {
+        if ( x < 0 || x > 2 )
+            return false;
+        return true;
+    }
+
+    public static Boolean checkValidPlayerOption( String p ) {
+        if ( p.equals( "1" ) || p.equals( "2" ) )
+            return true;
+        return false;
     }
 
 
 }
 
-class SimpleRunner implements Runnable{
+class SimpleRunner implements Runnable {
 
     @Override
     public void run() {
